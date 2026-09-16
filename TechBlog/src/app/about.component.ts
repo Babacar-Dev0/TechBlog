c3f38e167bf6106436943db96e0ef946a9a951ba
Abdou kebe <kebegeneral457@gmail.com>
2026-09-16 13:47:14 +0200

    Ajout du fichier about.component.ts

--- TechBlog/src/app/about.component.ts
+++ TechBlog/src/app/about.component.ts
@@ -5,16 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <div class="container">
-      <h1>À propos de TechBlog</h1>
-      <p>
-        TechBlog est un blog créé dans le cadre du cours de Technologie Web 3,
-        pour partager des articles courts sur le développement web.
-      </p>
-      <h2>Le binôme</h2>
-      <p>Prénom NOM 1 — rôle</p>
-      <p>Prénom NOM 2 — rôle</p>
-    </div>
-  `
+  <div class="container">
+    <h1>À propos de TechBlog</h1>
+    <p>
+      TechBlog est un blog créé dans le cadre du cours de Technologie Web 3,
+      pour partager des articles courts sur le développement web.
+    </p>
+    <h2>Le binôme</h2>
+    <p>Babacar Ndiaye — Configuration du projet, structure des fichiers racine, routage</p>
+    <p>Abdou Kebe — Composants (navbar, footer, pages), service de données</p>
+  </div>
+`
+    
+  
 })
 export class AboutComponent {}
